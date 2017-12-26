c54358679efc4f9935b14dc73674ed405fcca37f
JCGrant <jamescolin.grant@gmail.com>
2017-12-26 21:44:29 +0000

    Display a lit of todos
    Remove App.css and logo.svg

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,30 @@
 import * as React from 'react';
-import './App.css';
 
-const logo = require('./logo.svg');
+const initialState = [
+  {
+    id: 0,
+    text: 'wash dishes',
+    done: false,
+  },
+  {
+    id: 1,
+    text: 'write down thoughts',
+    done: false,
+  },
+  {
+    id: 2,
+    text: 'buy flowers for girlfriend',
+    done: false,
+  },
+];
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+      <div>
+        <ul>
+          {initialState.map((todo) => <li key={todo.id}>{todo.text}</li>)}
+        </ul>
       </div>
     );
   }
