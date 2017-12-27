1bd1f975b35567459fe5dc606741ca54220da393
JCGrant <jamescolin.grant@gmail.com>
2017-12-27 00:23:32 +0000

    Define ToggleTodo action

--- src/reducers.ts
+++ src/reducers.ts
@@ -1,10 +1,42 @@
 import { combineReducers } from 'redux';
 import { TodoData } from './App';
 
+enum ActionType {
+  TOGGLE_TODO = 'TOGGLE_TODO',
+}
+
+interface ToggleTodo {
+  type: ActionType.TOGGLE_TODO;
+  payload: {
+    id: number;
+  };
+}
+
+export const todoActions = {
+  toggleTodo: (id: number): ToggleTodo => ({
+    type: ActionType.TOGGLE_TODO,
+    payload: {
+      id,
+    },
+  }),
+};
+
+type Action =
+  | ToggleTodo
+  ;
+
 interface TodoState extends TodoData {}
 
-const todos = (state: TodoState[] = []) => {
-  return state;
+const todos = (state: TodoState[] = [], action: Action) => {
+  switch (action.type) {
+    case ActionType.TOGGLE_TODO:
+      return state.map((todo) => todo.id !== action.payload.id ? todo : {
+        ...todo,
+        done: !todo.done,
+      });
+    default:
+      return state;
+  }
 };
 
 export interface AppState {
